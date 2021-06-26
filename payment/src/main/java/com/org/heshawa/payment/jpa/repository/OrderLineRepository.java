b236c06ae9f8db1b3cd6f2f40af88cad7fd8f79c
heshawa <56378971+heshawa@users.noreply.github.com>
2021-06-27 01:38:34 +0530

    Delete payment directory

--- payment/src/main/java/com/org/heshawa/payment/jpa/repository/OrderLineRepository.java
+++
@@ -1,7 +0,0 @@
-package com.org.heshawa.payment.jpa.repository;
-
-import com.org.heshawa.payment.model.OrderLine;
-import org.springframework.data.repository.CrudRepository;
-
-public interface OrderLineRepository extends CrudRepository<OrderLine,String> {
-}
--- payment/src/main/java/com/org/heshawa/payment/jpa/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.org.heshawa.payment.jpa.repository;
-
-import com.org.heshawa.payment.model.Order;
-import org.springframework.data.repository.CrudRepository;
-
-public interface OrderRepository extends CrudRepository<Order,String> {
-}
